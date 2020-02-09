25ad4ba32004e611c3271af0d065b2bda7284a04
Nicholas Juniper <njuniper99@gmail.com>
2020-02-09 11:48:28 -0500

    Added Networking package and implemented

---
+++ Assets/Scripts/Networking/WatchedObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WatchedObject : MonoBehaviour
+{
+    public void SendPosition()
+    {
+        //Send position
+        NetworkManager.SendFloat(transform.position.x);
+        NetworkManager.SendFloat(transform.position.y);
+        NetworkManager.SendFloat(transform.position.z);
+    }
+}
