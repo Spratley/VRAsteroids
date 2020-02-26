00dd86fba1a95d7ee93b9e1a4c5ef70dacfc2c34
Jacob Pratley <pratleyjacob@gmail.com>
2020-02-26 17:13:32 -0500

    Implemented Physics Joystick

---
+++ Assets/PhysicsButtons/Scripts/PhysicsJoystick.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum JoystickAxis { X, Y }
+
+public class PhysicsJoystick : PhysicsDevice
+{
+    [Space]
+    public GameObject handle;
+
+    public JoystickAxis axis;
+
+    public override void UpdateValue()
+    {
+        base.UpdateValue();
+
+        float val;
+        switch(axis)
+        {
+            default:
+            case JoystickAxis.X:
+                val = handle.transform.localRotation.x;
+                break;
+            case JoystickAxis.Y:
+                val = handle.transform.localRotation.z;
+                break;
+        }
+
+        value = val;
+    }
+}
---
+++ Assets/Scripts/MatchChildLayers.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchChildLayers : MonoBehaviour
+{
+    int layerToMatch;
+
+    private void Start()
+    {
+        layerToMatch = gameObject.layer;
+
+        DepthSearch(gameObject);
+    }
+
+    void DepthSearch(GameObject currentObj)
+    {
+        currentObj.layer = layerToMatch;
+
+        var children = GetChildren(currentObj);
+
+        foreach(var child in children)
+        {
+            if (child.gameObject == currentObj)
+                continue;
+
+            DepthSearch(child.gameObject);
+        }
+    }
+
+    Transform[] GetChildren(GameObject parent)
+    {
+        return parent.GetComponentsInChildren<Transform>();
+    }
+}
