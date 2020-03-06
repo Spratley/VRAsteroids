d731850f59a8bb6eea0a775c57e5deb533363deb
Jacob Pratley <pratleyjacob@gmail.com>
2020-03-06 08:56:04 -0500

    Physics joystick completed (finally lol)

--- Assets/PhysicsButtons/Scripts/PhysicsJoystick.cs
+++ Assets/PhysicsButtons/Scripts/PhysicsJoystick.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public enum JoystickAxis { X, Y }
 
@@ -11,22 +12,49 @@ public class PhysicsJoystick : PhysicsDevice
 
     public JoystickAxis axis;
 
+    public float angleLimit;
+
+    private float initialOffset;
+
+    private void Start()
+    {
+        switch (axis)
+        {
+            default:
+            case JoystickAxis.X:
+                initialOffset = handle.transform.localEulerAngles.x;
+                break;
+            case JoystickAxis.Y:
+                initialOffset = handle.transform.localEulerAngles.z;
+                break;
+        }
+    }
+
     public override void UpdateValue()
     {
         base.UpdateValue();
 
         float val;
-        switch(axis)
+        Vector3 direction = handle.transform.forward;
+
+        switch (axis)
         {
             default:
             case JoystickAxis.X:
-                val = handle.transform.localRotation.x;
+                direction -= Vector3.Dot(direction, transform.right) * transform.right;
+                val = Mathf.Sign(Vector3.Dot(direction, transform.forward));
                 break;
             case JoystickAxis.Y:
-                val = handle.transform.localRotation.z;
+                direction -= Vector3.Dot(direction, transform.forward) * transform.forward;
+                val = Mathf.Sign(Vector3.Dot(direction, transform.right));
                 break;
         }
 
+        val *= Vector3.Angle(transform.up, direction);
+        val /= angleLimit;
+        val /= 2;
+        val += 0.5f;
+        
         value = val;
     }
 }
