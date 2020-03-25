a3a2c1943d53696cd0beabbb18e4dcca7d5409f9
Nicholas Juniper <njuniper99@gmail.com>
2020-03-25 14:59:04 -0400

    Possibly moving ships on network? Let's test

---
+++ Assets/Scripts/Networking/MeteorRushGameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using UnityEngine;
+
+public class MeteorRushGameManager : MonoBehaviour
+{
+    public ShipComponents m_player1;
+    public ShipComponents m_player2;
+
+    private void Awake()
+    {
+        if (!GameManager.m_multiplayer)
+        {
+            m_player2.gameObject.SetActive(false);
+        }
+        else
+        {
+            if (GameManager.m_player2) 
+            {
+                m_player1.m_shipInterior.SetActive(false);
+                m_player2.m_shipInterior.SetActive(true);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Networking/ShipComponents.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipComponents : MonoBehaviour
+{
+    public GameObject m_shipInterior;
+    public GameObject m_shipGraphics;
+}
